3f1520c7ea37687d88d29edb1535933341b1a048
Sanngrithr <34239794+Sanngrithr@users.noreply.github.com>
2023-09-27 19:57:12 +0000

    ich wollte cool sein wie Theo und Tailwindcss zum stylen benutzen, aber musste ich erneut festsellen, dass ich css immer noch hasse und jetzt habe ich wieder ptsd... gute nacht :*

---
+++ sensor-next/src/pages/BarChart.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
+interface DataItem {
+  name: string;
+  value: number;
+  color: string;
+}
+
+const data: DataItem[] = [
+  { name: 'Category 1', value: 10, color: '#8884d8'},
+  { name: 'Category 2', value: 20, color: '#82ca9d'},
+  { name: 'Category 3', value: 15, color: '#ffc658'},
+  { name: 'Category 4', value: 25, color: '#d84a68'},
+  { name: 'Category 5', value: 30, color: '#a3a0fb'},
+];
+
+const BarChartComponent: React.FC = () => {
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <BarChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        {data.map((entry) => (
+          <Bar
+            key={entry.value}
+            dataKey="value"
+            fill={entry.color} // Set bar color
+            label={{ position: 'top' }}
+          />
+        ))}
+      </BarChart>
+    </ResponsiveContainer>
+  );
+};
+
+export default BarChartComponent;
---
+++ sensor-next/src/pages/Table.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+interface TableRow {
+  column1: string;
+  column2: string;
+  column3: string;
+}
+
+const Table: React.FC = () => {
+  // Generate random data for the table
+  const tableData: TableRow[] = [
+    { column1: 'Value 1-1', column2: 'Value 1-2', column3: 'Value 1-3' },
+    { column1: 'Value 2-1', column2: 'Value 2-2', column3: 'Value 2-3' },
+    { column1: 'Value 3-1', column2: 'Value 3-2', column3: 'Value 3-3' },
+    { column1: 'Value 4-1', column2: 'Value 4-2', column3: 'Value 4-3' },
+    { column1: 'Value 5-1', column2: 'Value 5-2', column3: 'Value 5-3' },
+  ];
+
+  return (
+    <div className="table text-center text-sm text-indigo-700 font-extralight">
+      <table>
+        <thead>
+          <tr>
+            <th>Timestamp</th>
+            <th>Name</th>
+            <th>Wert</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((row, index) => (
+            <tr key={index}>
+              <td>{row.column1}</td>
+              <td>{row.column2}</td>
+              <td>{row.column3}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Table;
